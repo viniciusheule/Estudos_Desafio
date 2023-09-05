a48f752fde92ab1ea2d2dcdf62f49a8c1fd9358a
Vinicius Heule <viniciusheule@gmail.com>
2023-09-05 17:08:08 -0300

    feat: create Notification

---
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,26 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.NotificationDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("erro ao enviar notificação");
+            throw new Exception("Serviço de notificação fora do ar");
+        }
+    }
+}
